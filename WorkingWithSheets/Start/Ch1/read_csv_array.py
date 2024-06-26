b8fec464cb08aa484ef6b8e0a3f4dcb0b8dc100f
emZubair <zubair1714@gmail.com>
2024-06-26 18:16:07 +0500

    CSV reader, read file as Array & Dict

--- WorkingWithSheets/Start/Ch1/read_csv_array.py
+++ WorkingWithSheets/Start/Ch1/read_csv_array.py
@@ -1,16 +1,28 @@
 # LinkedIn Learning Course
-# Example file for Python: Working with Excel and Spreadsheet Data by Joe Marini
+# Example file for Python: Working with Excel and Spreadsheet Data by Joe Marini.
 # Reading CSV file into an array
 
 # import the csv module from the standard library
 import csv
+from pathlib import Path
+
 
 def read_csv_to_array(filename):
-  # define the array that will hold the data
-  data = []
+    p = Path(__file__).with_name(filename)
+    data = []
+    with p.open("r") as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            data.append(row)
+        return data
+
 
 # Read the data into an array of arrays
 inventory_data = read_csv_to_array("Inventory.csv")
 
 # Each row in the array is itself an array of values
-
+print(f"Read rows: {len(inventory_data)}")
+print(inventory_data[0])
+print(inventory_data[1])
+print(f"Break down of \n{inventory_data[3]}")
+print(f"Name:{inventory_data[3][0]}, Quantity:{inventory_data[3][2]}")
--- WorkingWithSheets/Start/Ch1/read_csv_dict.py
+++ WorkingWithSheets/Start/Ch1/read_csv_dict.py
@@ -3,14 +3,23 @@
 # Reading CSV file into an dictionary
 
 import csv
-import pprint
+from pathlib import Path
+from pprint import pprint
+
 
 def read_csv_to_dict(filename):
-  data = {}
+    file_path = Path(__file__).with_name(filename)
+    data = {}
+    with file_path.open("r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        row = next(reader)
+        for row in reader:
+            data[row[reader.fieldnames[0]]] = row
+    return data
 
-  return data
 
-# Example usage
 inventory_data = read_csv_to_dict("Inventory.csv")
 
-# Accessing data
+pprint(inventory_data)
+pprint(inventory_data["Pineapple"])
+pprint(inventory_data["Pineapple"]["Consumer Price"])
